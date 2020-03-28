a8fdb574316e92b217d2730cc82aff33b1f7f316
alexanderqchen <aqchen@ucla.edu>
2020-03-28 13:18:56 -0700

    receiving transcripts of calls now works

--- playground/receive-call.js
+++ playground/receive-call.js
@@ -6,17 +6,21 @@ const VoiceResponse = require("twilio").twiml.VoiceResponse;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+let toNumber;
+let recordingInitiated = false;
+let recordingFinished = false;
+let transcription = "";
+
 const gatherPhoneNumber = twiml => {
   const gather = twiml.gather({ numDigits: 10 });
   gather.say("What phone number would you like to call?");
-  twiml.redirect("/voice");
 };
 
 const recordName = twiml => {
   twiml.say("What is your name?");
   twiml.record({
     transcribe: true,
-    transcribeCallback: "/voice",
+    transcribeCallback: "/handle",
     finishOnKey: "*"
   });
 };
@@ -24,27 +28,33 @@ const recordName = twiml => {
 app.post("/voice", (req, res) => {
   const twiml = new VoiceResponse();
 
-  // if (req.body.Digits === undefined) {
-  //   gatherPhoneNumber(twiml);
-  // }
-  console.log("in /voice");
-
-  if (req.body.TranscriptionText === undefined) {
-    recordName(twiml);
+  if (req.body.Digits !== undefined) {
+    toNumber = req.body.Digits;
   }
 
-  console.log(`Hello, ${req.body.TranscriptionText}`);
-
-  // const toNumber = req.body.Digits;
-
-  // twiml.say(`The phone number you entered is ${req.body.Digits}`);
+  if (toNumber === undefined) {
+    gatherPhoneNumber(twiml);
+  } else if (!recordingInitiated) {
+    recordingInitiated = true;
+    recordName(twiml);
+  } else if (!recordingFinished) {
+    twiml.say("Wait a moment while your response is being processed.");
+    twiml.pause({ length: 10 });
+    twiml.redirect("/voice");
+  } else {
+    twiml.say(transcription);
+    toNumber = undefined;
+    recordingInitiated = false;
+    recordingFinished = false;
+  }
 
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
 
 app.post("/handle", (req, res) => {
-  console.log(req.body.TranscriptionText);
+  recordingFinished = true;
+  transcription = req.body.TranscriptionText;
 });
 
 http.createServer(app).listen(1337, () => {
